e55771973809cf56af3403e8bc7e19890ab85a62
Peter Rogers <peter.rogers@gmail.com>
2018-06-22 14:38:12 -0400

    Added header copyright notices and updated README

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # NES High Scorer
 
-Publishes high scores achieved in NES games
+Publishes high scores achieved in NES games.
+
+This code is designed to work with a fork of libretro-fceu, the NES emulation
+plugin for retroarch. The forked plugin is here:
+
+https://github.com/parogers/libretro-fceumm
+
+# How it works
+
+The retroarch plugin is modified to create the NES RAM as a block of shared
+memory, in the filesystem under '/dev/shm/fceu-shm'. This allows external
+processes to directly read/write the contents of NES memory, letting us extract
+things like high-scores.
+
+An external python script periodically reads NES RAM and waits for the high
+score table to be updated. The new score is then tweeted out.
 
 ## License
 
--- nes/tetris.py
+++ nes/tetris.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+# nes-high-scorer -- tweets out high-scores achieved in NES games
+# Copyright (C) 2018  Peter Rogers (peter.rogers@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import fceu
 from collections import namedtuple
@@ -24,6 +38,12 @@ HIGH_SCORES_B = (
     HighScoreEntry(0x71d, 0x73f, 0x74d),
     HighScoreEntry(0x724, 0x742, 0x74e),
 )
+
+VERTICAL_POS = 0x61
+NEXT_PIECE = 0x62
+CURRENT_PIECE = 0xbf
+
+LINE_PIECE = 18
 
 def get_high_scores_by_table(ram, table):
     lst = []
