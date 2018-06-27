9590693121f2d02c9f6f97e25d226febe5dbc7cc
Peter Rogers <peter.rogers@gmail.com>
2018-06-27 12:38:22 -0400

    Added script for extracting tetris high scores (debugging)

---
+++ tools/get_high_scores.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import nes, nes.fceu, nes.tetris
+
+ram = nes.fceu.open_shm()
+for entry in nes.tetris.get_high_scores(ram):
+    print(repr(entry.name), entry.score, entry.level, entry.game_type)
+
